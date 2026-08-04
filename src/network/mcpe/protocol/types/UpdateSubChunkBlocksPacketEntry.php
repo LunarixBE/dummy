f56f7379b5f07e43c8508bee8ca0a71a81942d42
xan6x <131452682+xan6x@users.noreply.github.com>
2026-08-04 11:07:09 +0400

    fix

--- src/network/mcpe/protocol/types/UpdateSubChunkBlocksPacketEntry.php
+++ src/network/mcpe/protocol/types/UpdateSubChunkBlocksPacketEntry.php
@@ -23,6 +23,7 @@ declare(strict_types=1);
 
 namespace pocketmine\network\mcpe\protocol\types;
 
+use pocketmine\network\mcpe\protocol\ProtocolInfo;
 use pocketmine\network\mcpe\protocol\serializer\PacketSerializer;
 use pocketmine\network\mcpe\protocol\UpdateBlockPacket;
 
